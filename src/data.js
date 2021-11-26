def3ae575b8700f00db89a15a5f7d1ac92073a35
Martin <martin.duconseil@gmail.com>
2021-11-26 16:24:12 +0100

    Upload de la nouvelle version

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from './assets/img/pomme.jpeg';
+import img2 from './assets/img/banane.jpeg';
+import img3 from './assets/img/peche.jpeg';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Pomme',
+            desc: '',
+            price: 15,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Banane',
+            desc: '',
+            price: 4,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Pêche',
+            desc: '',
+            price: 7,
+        }
+    ],
+};
+
+export default data;
