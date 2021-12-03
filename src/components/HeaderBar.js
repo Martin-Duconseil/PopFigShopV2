848deb8601936a79fdf2ab0d8d4cbf7a223de60f
Martin <martin.duconseil@gmail.com>
2021-12-03 12:28:25 +0100

    Maj card product

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class HeaderBar extends React.Component {
 
     render() {
-        return <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <a className="navbar-brand" href="/">PopFigShop</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -16,5 +16,9 @@ export default class HeaderBar extends React.Component {
                 </div>
             </div>
         </nav>
+
+        
     }
 }
+
+
--- src/routes/product.jsx
+++ src/routes/product.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router";
 import { useEffect } from "react";
 import data from "../data/data";
+import { Badge } from "react-bootstrap";
+
 
 
 
@@ -8,67 +10,93 @@ export default function Product() {
 
     useEffect(() => {
         window.scrollTo(0, 0)
-      }, [])
+    }, [])
 
     const { productSlug } = useParams()
-    
+
     const pageProduct = data.productData.find((product) => {
         return productSlug === product.slug
     })
-    
+
     if (!pageProduct) {
         return "Oups ! La produit que vous cherchez actuellement n'existe pas ou n'existe plus."
     }
 
     function isExclusive() {
         if (pageProduct.exclusivity != "") {
-            return <p>Exclusif : {pageProduct.exclusivity}</p>;
+            return <li>Exclusif : {pageProduct.exclusivity}</li>;
         } else {
             return null;
         }
     }
 
-
     
-        return (
-<<<<<<< HEAD
-    <section className="section-content padding-y bg">
-        <div className="container">
 
-            <article className ="card">
-                <div className ="card-body">
-                    <div class="row">
-                        <aside class="col-md-6">
-                            <article class="gallery-wrap">
-                                <article class="card img-big-wrap">
-                                                <img src={pageProduct.img} class="rounded mx-auto d-block" />
-                                </article>                
-                                                <h3>{pageProduct.title} #{pageProduct.number}</h3>
-                                                <p>Référence Funko : {pageProduct.reference}</p>
-                                                
-                                                <p>Licence : {pageProduct.licence}</p>
-                                                <p>Version : {pageProduct.version}</p>
-                                                <p>Année : {pageProduct.year}</p>
-                                                <p>Exclusif : {pageProduct.exclusivity}</p>
-                                                <p>Description : {pageProduct.desc}</p>
-                                
-                            </article>
-                        </aside>
+    return (
+        <section className="section-content padding-y bg">
+            <div className="container">
+
+                <article className="card">
+                    <div className="card-body">
+                        <div class="row">
+                            <aside class="col-md-6">
+                                <article class="gallery-wrap">
+                                    <div class="card img-big-wrap">
+                                        <img src={pageProduct.img} class="rounded mx-auto d-block" />
+                                    </div>
+                                </article>
+                            </aside>
+                            <main class="col-md-6">
+                                <article>
+                                    <br />
+                                    <h3>{pageProduct.title} #{pageProduct.number}
+                                    </h3>
+                                    <Badge pill bg="primary">Funko N° {pageProduct.reference}</Badge>
+                                    {' '}
+                                    <Badge pill bg="secondary">{pageProduct.licence}</Badge>
+                                    <hr />
+
+                                    <div class="mb-3">
+                                        <h6>Caractéristiques</h6>
+                                        <ul class="list-dots mb-0">
+                                            <li>Licence : {pageProduct.licence}</li>
+                                            <li>Version : {pageProduct.version}</li>
+                                            <li>Année : {pageProduct.year}</li>
+                                            <p>{isExclusive()}</p>
+
+                                        </ul>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <var class="price h4">{pageProduct.price} €</var> <br />
+                                    </div>
+                            
+                                    <div class="mb-4">
+                                        <a href="#" class="btn btn-primary mr-1">Ajouter au panier</a>
+                                        
+                                    </div>
+
+                                </article>
+                            </main>
+                        </div>
                     </div>
-                </div>
-            </article>
-=======
-        <div>
-            <h3>{pageProduct.title} #{pageProduct.number}</h3>
-            <p>Référence Funko : {pageProduct.reference}</p>
-            <img src={pageProduct.img} />
-            <p>Licence : {pageProduct.licence}</p>
-            <p>Version : {pageProduct.version}</p>
-            <p>Année : {pageProduct.year}</p>
-            <p>{isExclusive()}</p>
-            <p>Description : {pageProduct.desc}</p>
->>>>>>> 00759b116fae13789913d8c0d8bc39377aa6cb8b
-        </div>
-    </section>
+                </article>
+                <article class="card mt-5">
+                    <div class="card-body">
+                        <div class="row">
+                            <h2>Description</h2>
+                        </div>
+                        <hr />
+                        <p>
+                            {pageProduct.desc}
+                        </p>
+                    </div>
+                </article>
+            </div>
+        </section>
+
+
+
+
     );
 }
